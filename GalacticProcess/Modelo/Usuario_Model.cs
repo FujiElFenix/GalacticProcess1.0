482aab8af510cca0c44ed31dffe8a8a7732d428f
jose_hxc-12 <jose_hxc-12@hotmail.com>
2019-09-28 03:25:11 -0300

    +comentarios

--- GalacticProcess/Modelo/Usuario_Model.cs
+++ GalacticProcess/Modelo/Usuario_Model.cs
@@ -23,14 +23,16 @@ namespace GalacticProcess.Modelo
         {
             try
             {
+                
                 OracleCommand cmd = new OracleCommand();
+                
                 cmd.CommandText = "LOGIN_ESCRITORIO";
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add("PRM_USER", usuario.Username);
                 cmd.Parameters.Add("PRM_PASS", usuario.Password);
                 cmd.Parameters.Add("TIPO_USUARIO", OracleType.Number);
                 cmd.Parameters["TIPO_USUARIO"].Direction = ParameterDirection.ReturnValue;
-               // cmd.Connection = conexion.Conectar();
+                cmd.Connection = conexion.Conectar();
                 cmd.ExecuteNonQuery();
                 int tipo_usuario = int.Parse(Convert.ToString(cmd.Parameters["TIPO_USUARIO"].Value));
                 return tipo_usuario;
@@ -38,6 +40,10 @@ namespace GalacticProcess.Modelo
             catch(Exception e)
             {
                 return 0;
+            }
+            finally
+            {
+                conexion.CerrarConexion();
             }
         }
     }
--- GalacticProcess/Program.cs
+++ GalacticProcess/Program.cs
@@ -17,7 +17,7 @@ namespace GalacticProcess
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ModuloEmpresas());
+            Application.Run(new Login());
         }
     }
 }
